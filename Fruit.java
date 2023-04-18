e515be5b55326a0b907b07d6f25950ac1c655e4c
Sergey <kashtanov_sv@bk.ru>
2023-04-18 18:10:45 +0300

    Les3

---
+++ Fruit.java
@@ -0,0 +1,5 @@
+package project.by.Kashtanov;
+
+public class Fruit {
+    abstract float getWeight();
+}
---
+++ Orange.java
@@ -0,0 +1,7 @@
+package project.by.Kashtanov;
+
+public class Orange extends Fruit{
+    float getWeightgetWeight() {
+        return 1.5f;
+    }
+}
